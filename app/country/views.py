54c1d87581c2be8615fd60b400961099c79440e5
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2020-07-01 09:04:41 +0100

    Re-fectored Country and State Views

--- app/country/views.py
+++ app/country/views.py
@@ -7,35 +7,28 @@ from core.models import Country, State
 from country import serializers
 
 
-class CountryViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                     mixins.CreateModelMixin):
-    '''Manage countries in the database'''
+class BaseCountryAttrViewset(viewsets.GenericViewSet, mixins.ListModelMixin,
+                             mixins.CreateModelMixin):
+    '''Base viewset for user owned country attributes'''
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        '''Returns objects for the current authenticated user'''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        '''Create a new object'''
+        serializer.save(user=self.request.user)
+
+
+class CountryViewSet(BaseCountryAttrViewset):
+    '''Manage countries in the database'''
     queryset = Country.objects.all()
     serializer_class = serializers.CountrySerializer
 
-    def get_queryset(self):
-        '''Returns objects for the current authenticated user only'''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        '''Create a new country'''
-        serializer.save(user=self.request.user)
-
-
-class StateViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                   mixins.CreateModelMixin):
+class StateViewSet(BaseCountryAttrViewset):
     '''Manage states in the database'''
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = State.objects.all()
     serializer_class = serializers.StateSerializer
-
-    def get_queryset(self):
-        '''Returns object for the current authenticated user'''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        '''Create a new state'''
-        serializer.save(user=self.request.user)
